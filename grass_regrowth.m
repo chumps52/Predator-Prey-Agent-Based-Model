26c09db40c5f068c8703f356469ac2a0f3894894
Catalin Nicolae Mares <cmares1@sheffield.ac.uk>
2019-03-11 10:47:46 +0000

    Introducing grass regrowth global parameter

--- grass_regrowth.m
+++ grass_regrowth.m
@@ -2,11 +2,12 @@ function grass_regrowth()
 
 %Function regrows grass every 4 weeks by 10 units
 %For each cell in the grid the grass is grown by 10 units
-global ENV_DATA
+global ENV_DATA PARAM
+
 [r,c] = size(ENV_DATA.food); 
 for row = 1:r
     for column = 1:c     
-        ENV_DATA.food(row,column) = ENV_DATA.food(row,column)+10;
+        ENV_DATA.food(row,column) = ENV_DATA.food(row,column)+PARAM.GRASS_REGROWTH;
     end
 end
 end
